7cdba7012cef9b983ad87dc8e60a5cb2ed0ee51d
baihaki7 <haki000015@gmail.com>
2025-04-27 23:23:59 +0800

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,12 @@
+print ("muhamad baihaki 242421128")
+inputuser = int(input("masukkan bilangan antara 24 atau lebih dari 28"))
+
+#+++++24------------
+#memeriksa angka yang kurang dari 24
+iskurangdari = (inputuser <24)
+
+#memeriksa angka yang kurang dari 28
+islebihdari = (inputuser <28)
+
+final = iskurangdari or islebihdari
+print("angka yang anda masukan", final)
